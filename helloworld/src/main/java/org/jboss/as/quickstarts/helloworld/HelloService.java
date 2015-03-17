c31e7739946736f2395bc002164c03602f043e47
Vladislav <wladitslove@gmail.com>
2015-03-17 17:42:13 +0300

    Update HelloService.java

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello jenkins&jboss&me " + name + "!";
+        return "Hello jenkins&jb" + name + "!";
     }
 
 }
